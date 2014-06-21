3acaf4a28d0df81b2adeb1539ff3ca9efb4b038f
Franzo- <filippo.franzoni@studio.unibo.it>
2014-06-22 01:15:16 +0200

    Aggiunto codice e diagramma di Classifica

---
+++ Prototipo/WindowsFormsApplication1/Model/Classifica.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketSystem.Model
+{
+    class Classifica
+    {
+        private IEnumerable<Statistica> _statistiche;
+        private ISelezionatore _selezionatore;
+
+        public event EventHandler Changed;
+
+        public ISelezionatore Selezionatore
+        {
+            get { return _selezionatore; }
+            set
+            {
+                if (value != null && value != _selezionatore)
+                {
+                    _selezionatore = value;
+                    Invalidate();
+                }
+            }
+        }
+
+        public IEnumerable<Statistica> GetStatistiche() 
+        {
+            if (_statistiche == null)
+            {
+                _statistiche = _selezionatore.GetStatistiche();
+            }
+            return _statistiche;
+        }
+
+        public void Invalidate()
+        {
+            _statistiche = null;
+            OnChanged();
+        }
+
+        private void OnChanged()
+        {
+            if (Changed != null)
+                Changed(this, EventArgs.Empty);
+        }
+    }
+}
